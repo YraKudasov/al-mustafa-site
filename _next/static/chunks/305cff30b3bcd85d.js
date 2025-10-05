2b232349ca77dd4f25f7907601d3df735dc435ed
Yra <kudasov5@mail.ru>
2025-10-05 19:38:31 +0300

    version 1.0.1

---
+++ _next/static/chunks/305cff30b3bcd85d.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/4ef1c88d00ffbc51.js",
+  "static/chunks/turbopack-ae587208d6338c29.js"
+])
---
+++ _next/static/chunks/394c229728458099.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/4ef1c88d00ffbc51.js",
+  "static/chunks/turbopack-af40cc981a75b55e.js"
+])
